4b9cc89275d9ff4452f0fbdbb9f644f35a8666fd
Sayyad Gul Mohammad <syedgulmohammad25794@gmail.com>
2025-05-12 07:12:33 +0530

    added log

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button"
 import Link from "next/link"
 
 export default function Home() {
+  console.log('DATABASE_URL in prod:', process.env.DATABASE_URL);
   return (
     <div className="flex min-h-screen flex-col">
       <header className="bg-background border-b">
